a8c7d2023911c003b3f945a6ad16fb9dc4ef333b
derrick <kang.derrick@gmail.com>
2015-07-30 21:43:56 +0900

    test

---
+++ samplecode/parsingtest/helloworld/helloworld/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  helloworld
+//
+//  Created by 강진영 on 7/30/15.
+//  Copyright (c) 2015 sensation. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
