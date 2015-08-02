a311e94f8a6ee6c53eff73dc78cc7db74d331310
derrick <kang.derrick@gmail.com>
2015-08-02 17:25:05 +0900

    add test simple code to test read or write as text

---
+++ samplecode/readwritetext/readwritetext/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  readwritetext
+//
+//  Created by 강진영 on 8/2/15.
+//  Copyright (c) 2015 sensation. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
