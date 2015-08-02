93cd7a78385a30691130a1035001a1f62f88fe1b
derrick <kang.derrick@gmail.com>
2015-08-02 18:37:23 +0900

    add samplecode to test normal textfile writing work

--- samplecode/readwritetext/readwritetext/main.c
+++ samplecode/readwritetext/readwritetext/main.c
@@ -8,8 +8,39 @@
 
 #include <stdio.h>
 
+struct people{
+    char name[10];
+    char job[10];
+    int age;
+    char city[10];
+};
+
+
 int main(int argc, const char * argv[]) {
     // insert code here...
-    printf("Hello, World!\n");
+    
+    struct people person[3];
+    int i;
+    FILE *f;
+    strcpy(person[0].name,"Derrick");
+    person[0].age=15;
+    strcpy(person[0].job,"Software Engineer");
+    strcpy(person[0].city,"Seoul");
+    
+  
+    
+    for(i=0;i<1;i++)
+        printf("%s %d %s %s\n",person[i].name,person[i].age, person[i].job, person[i].city);
+    
+    
+    f=fopen("a.txt","w");
+    
+    for(i=0;i<1;i++)
+        fprintf(f,"%s %d %s %s\n",person[i].name,person[i].age, person[i].job, person[i].city);
+    
+    
+    fclose(f);
+    
     return 0;
+
 }
