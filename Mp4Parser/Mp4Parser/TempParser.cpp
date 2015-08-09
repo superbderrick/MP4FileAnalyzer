a2b7a4f602b0268b3c4ee5659e64aa72003b246e
derrick <kang.derrick@gmail.com>
2015-08-09 14:24:08 +0900

    added module that can read file size

--- Mp4Parser/Mp4Parser/TempParser.cpp
+++ Mp4Parser/Mp4Parser/TempParser.cpp
@@ -1,9 +1,9 @@
 //
-//  Mp4Parser.cpp
+//  TempParser.cpp
 //  Mp4Parser
 //
 //  Created by 강진영 on 8/8/15.
 //  Copyright (c) 2015 sensation. All rights reserved.
 //
 
-#include "Mp4Parser.h"
+#include "TempParser.h"
--- Mp4Parser/Mp4Parser/TempParser.h
+++ Mp4Parser/Mp4Parser/TempParser.h
@@ -1,14 +1,14 @@
 //
-//  Mp4Parser.h
+//  TempParser.h
 //  Mp4Parser
 //
 //  Created by 강진영 on 8/8/15.
 //  Copyright (c) 2015 sensation. All rights reserved.
 //
 
-#ifndef __Mp4Parser__Mp4Parser__
-#define __Mp4Parser__Mp4Parser__
+#ifndef __Mp4Parser__TempParser__
+#define __Mp4Parser__TempParser__
 
 #include <stdio.h>
 
-#endif /* defined(__Mp4Parser__Mp4Parser__) */
+#endif /* defined(__Mp4Parser__TempParser__) */
