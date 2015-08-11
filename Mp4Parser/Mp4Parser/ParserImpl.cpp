a83001e67cc7a1a5f93a5fa6bd631c9edbf0f893
derrick <kang.derrick@gmail.com>
2015-08-12 01:34:56 +0900

    refestorying code

---
+++ Mp4Parser/Mp4Parser/ParserImpl.cpp
@@ -0,0 +1,9 @@
+//
+//  ParserImpl.cpp
+//  Mp4Parser
+//
+//  Created by 강진영 on 8/12/15.
+//  Copyright (c) 2015 sensation. All rights reserved.
+//
+
+#include "ParserImpl.h"
---
+++ Mp4Parser/Mp4Parser/ParserImpl.h
@@ -0,0 +1,14 @@
+//
+//  ParserImpl.h
+//  Mp4Parser
+//
+//  Created by 강진영 on 8/12/15.
+//  Copyright (c) 2015 sensation. All rights reserved.
+//
+
+#ifndef __Mp4Parser__ParserImpl__
+#define __Mp4Parser__ParserImpl__
+
+#include <stdio.h>
+
+#endif /* defined(__Mp4Parser__ParserImpl__) */
