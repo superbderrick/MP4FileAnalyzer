0752387a7a9393718cc75c950ac7f2a8a0be8aed
derrick <kang.derrick@gmail.com>
2015-08-13 01:52:52 +0900

    leave a function for  moov It will finish by tmoorow

---
+++ Mp4Parser/Mp4Parser/ContainerBox.cpp
@@ -0,0 +1,9 @@
+//
+//  ContainerBox.cpp
+//  Mp4Parser
+//
+//  Created by 강진영 on 8/13/15.
+//  Copyright (c) 2015 sensation. All rights reserved.
+//
+
+#include "ContainerBox.h"
---
+++ Mp4Parser/Mp4Parser/ContainerBox.h
@@ -0,0 +1,14 @@
+//
+//  ContainerBox.h
+//  Mp4Parser
+//
+//  Created by 강진영 on 8/13/15.
+//  Copyright (c) 2015 sensation. All rights reserved.
+//
+
+#ifndef __Mp4Parser__ContainerBox__
+#define __Mp4Parser__ContainerBox__
+
+#include <stdio.h>
+
+#endif /* defined(__Mp4Parser__ContainerBox__) */
